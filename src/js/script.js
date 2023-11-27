5188a0de009270b2246416ac4c942e7363fc0768
naaa <na0908nna@gmail.com>
2023-11-27 23:28:34 +0300

    javascript include home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,11 @@
 import './bootstrap';
 import '../css/app.css'
 
+// Carousel Ends     Initialization for ES Users
 
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let header = document.querySelector('.header-2');
-
-
-
-var MenuItems = document.getElementById("MenuItems");
-MenuItems.style.maxHeight = "0px";
-
-function menutoggle() {
-    if (MenuItems.style.maxHeight = "0px") {
-        MenuItems.style.maxHeight = "50px";
-    } else {
-        MenuItems.style.maxHeight = "0px";
-    }
-}
 
 /*************Menuitems JS***************/
 
@@ -30,16 +18,18 @@ function settingsMenuToggle() {
 }
 
 */
+var settingsmenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+
 var darkBtn = document.getElementById("dark-btn");
 
 darkBtn.onclick = function () {
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-theme");
-    if (localStorage.getItem("theme" == "light")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
+
     if (localStorage.getItem("theme" == "light")) {
         darkBtn.classList.remove("dark-btn-on");
         document.body.classList.remove("dark-theme");
@@ -47,6 +37,6 @@ darkBtn.onclick = function () {
         darkBtn.classList.add("dark-btn-on");
         document.body.classList.add("dark-theme");
     } else {
-        localStorage.setItem("theme", "light");
+        localStorage.setItem("theme", "light")
     }
 }
