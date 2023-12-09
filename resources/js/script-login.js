1ee00f6dbf1ace87997b14b4426ad95c5d0a9218
naaa <na0908nna@gmail.com>
2023-12-09 18:27:25 +0300

    Some change to Host
    host

--- resources/js/script-login.js
+++
@@ -1,8 +0,0 @@
-const labels = document.querySelectorAll('.form-control')
-
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((latter, idx) => '<span style"transitio-delay:${idx *50}ms">${letter}</span>')
-        .join('')
-})
